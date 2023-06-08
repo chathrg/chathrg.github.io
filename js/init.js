8d62685e1311b281e24d5ba480d6c3dd15476b2b
chathrg <135928480+chathrg@users.noreply.github.com>
2023-06-07 19:13:31 -0600

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyBrRyNGmMp3jlnY8UJG7rqNloIlXBPZBL8",
+  authDomain: "baseje43.firebaseapp.com",
+  projectId: "baseje43",
+  storageBucket: "baseje43.appspot.com",
+  messagingSenderId: "895307038299",
+  appId: "1:895307038299:web:8410bb5867e4317d62497e",
+  measurementId: "G-7W9B8BNGLZ"
+    });
